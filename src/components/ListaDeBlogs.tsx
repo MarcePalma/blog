f4982dd0653e95a5737e9b25a48847a2bf611aea
Marcelo Palma <marcelobaltazarpalma@gmail.com>
2023-11-05 14:10:30 -0300

    a

--- src/components/ListaDeBlogs.tsx
+++ src/components/ListaDeBlogs.tsx
@@ -24,36 +24,33 @@ export default function ListaDeBlogs() {
 
   return (
     <section>
-      {
-        blogs.map((blog) => (
-          <Link href={`/blogs/${blog.slug}`}>
-            <article className="overflow-hidden rounded-lg shadow transition hover:shadow-lg">
-              <img
-                alt="Office"
-                src="https://images.unsplash.com/photo-1524758631624-e2822e304c36?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
-                className="h-56 w-full object-cover"
-              />
+      {blogs.map((blog) => (
+        <Link key={blog.slug} href={`/blogs/${blog.slug}`}>
+          <article className="overflow-hidden rounded-lg shadow transition hover:shadow-lg">
+            <img
+              alt="Office"
+              src="https://images.unsplash.com/photo-1524758631624-e2822e304c36?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+              className="h-56 w-full object-cover"
+            />
 
-              <div className="bg-white p-4 sm:p-6">
-                <time className="block text-xs text-gray-500">
+            <div className="bg-white p-4 sm:p-6">
+              <time className="block text-xs text-gray-500">
                 {blog.meta.date}
-                </time>
+              </time>
 
-                <a href="#">
-                  <h3 className="mt-0.5 text-lg text-gray-900">
+              <a href="#">
+                <h3 className="mt-0.5 text-lg text-gray-900">
                   {blog.meta.title}
-                  </h3>
-                </a>
+                </h3>
+              </a>
 
-                <p className="mt-2 line-clamp-3 text-sm/relaxed text-gray-500">
-                  {blog.meta.description}
-                </p>
-              </div>
-            </article>
-          </Link>
-        ))
-
-      }
+              <p className="mt-2 line-clamp-3 text-sm/relaxed text-gray-500">
+                {blog.meta.description}
+              </p>
+            </div>
+          </article>
+        </Link>
+      ))}
     </section>
-  )
+  );
 }
